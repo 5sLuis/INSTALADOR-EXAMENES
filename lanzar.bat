76ef005df065d0cd2d3a85ef0c15fed695a847cb
5sLuis <luiscayancela18@gmail.com>
2026-06-07 11:46:42 -0500

    Update lanzar.bat

--- lanzar.bat
+++ lanzar.bat
@@ -1,15 +1,17 @@
 @echo off
 
 set URLFILE=%TEMP%\config.txt
-set TS=%RANDOM%%RANDOM%%RANDOM%
 
-powershell -Command "Invoke-WebRequest -UseBasicParsing -Headers @{'Cache-Control'='no-cache'} -Uri 'https://raw.githubusercontent.com/5sLuis/INSTALADOR-EXAMENES/main/config.txt?nocache=%TS%' -OutFile '%URLFILE%'"
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/5sLuis/INSTALADOR-EXAMENES/main/config.txt' -OutFile '%URLFILE%'"
 
 set /p URL=<"%URLFILE%"
 
 echo URL LEIDA: %URL%
 pause
 
-start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --new-window --kiosk "%URL%"
+taskkill /F /IM brave.exe >nul 2>&1
+timeout /t 2 >nul
+
+start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --user-data-dir="%TEMP%\ExamenBrave" --new-window --kiosk "%URL%"
 
 del "%URLFILE%"
