c2dc3a3636afd37a10e0d3cc63f3e33f84dd7f68
5sLuis <luiscayancela18@gmail.com>
2026-06-07 11:15:49 -0500

    Create lanzar.bat

---
+++ lanzar.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set URLFILE=%TEMP%\config.txt
+
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/5sLuis/INSTALADOR-EXAMENES/main/config.txt' -OutFile '%URLFILE%'"
+
+set /p URL=<"%URLFILE%"
+
+start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" --kiosk "%URL%"
+
+del "%URLFILE%"
