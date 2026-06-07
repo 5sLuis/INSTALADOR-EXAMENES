d5356407e37e3e1023aa1b1488bf9f35704a5971
5sLuis <luiscayancela18@gmail.com>
2026-06-07 12:00:19 -0500

    Update lanzar.bat

--- lanzar.bat
+++ lanzar.bat
@@ -5,6 +5,8 @@ set URLFILE=%TEMP%\config.txt
 powershell -Command "Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/5sLuis/INSTALADOR-EXAMENES/main/config.txt' -OutFile '%URLFILE%'"
 
 set /p URL=<"%URLFILE%"
+
+echo URL LEIDA: %URL%
 pause
 
 start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" "%URL%"
